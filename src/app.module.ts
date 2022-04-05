d5c16a939162dbd44b34046d637b2d4a7cfa3039
MoSanogo <modisalhydro@gmail.com>
2022-04-05 04:08:51 -0400

    end

---
+++ src/app.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ProductModule } from './product/product.module';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'micro_auth',
+      entities: ['dist/**/*.entity.{ts,js}'],
+      synchronize: true, // never true in production!
+     
+    }),
+    ProductModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
